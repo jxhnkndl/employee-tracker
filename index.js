90083be591d02f08458feb9240052a08ad280c95
jxhnkndl <jkroyston@gmail.com>
2021-02-22 13:01:16 -0500

    Add inquirer prompts for adding employee

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 // Import modules
 const mysql = require('mysql');
 const table = require('console.table');
+const inquirer = require('inquirer');
+
+// Employee roles array
+const roles = [
+  { value: 1, name: 'Chief Technology Officer' },
+  { value: 2, name: 'Senior Engineer' },
+  { value: 3, name: 'Junior Engineer' },
+  { value: 4, name: 'Data Analyst' },
+  { value: 5, name: 'Legal Team Lead' },
+  { value: 6, name: 'Associate Counsel' },
+  { value: 7, name: 'Chief Financial Officer' },
+  { value: 8, name: 'Accountant' },
+  { value: 9, name: 'Account Manager' },
+  { value: 10, name: 'Sales Team Lead' },
+  { value: 11, name: 'Sales Associate' },
+  { value: 12, name: 'Marketing Team Lead' },
+  { value: 13, name: 'Community Manager' },
+  { value: 14, name: 'Brand Ambassador' },
+];
+
+// Employee managers array
+const managers = [
+  { value: 1, name: 'J.K. Royston' },
+  { value: 2, name: 'Ford Wesson' },
+  { value: 3, name: 'Topher Talley' },
+  { value: 4, name: 'Jimmy Royston' },
+  { value: 5, name: 'Nicole Smith' },
+  { value: null, name: 'No Manager' },
+];
+
+// Init
+ask();
 
 // Primary logic routing function
 function ask() {
@@ -29,6 +61,7 @@ function ask() {
         case 'View All Employees':
           break;
         case 'Add New Employee':
+          addEmployee();
           break;
         case 'Update Employee Role':
           break;
@@ -47,4 +80,36 @@ function ask() {
           ask();
       }
     });
+}
+
+// Gather user input to add new employee to database
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: "Employee's First Name:",
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: "Employee's Last Name:",
+    },
+    {
+      type: 'list',
+      name: 'role_id',
+      choices: roles,
+      message: "Employee's Role:",
+    },
+    {
+      type: 'list',
+      name: 'manager_id',
+      choices: managers,
+      message: "Employee's Manager:",
+    },
+  ]).then(answers => {
+
+    console.log(answers);
+
+  });
 }
