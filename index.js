1b77e33c69e02df5febd6aeb7ce67336c3c090f6
jxhnkndl <jkroyston@gmail.com>
2021-02-23 15:05:14 -0500

    Convert viewData into a reusable, async/await function for querying data from the database

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const inquirer = require('inquirer');
 // Import object organizing SQL queries
 const query = require('./helpers/query');
 
+// Local data arrays (for adding dynamically inserted data into inquirer prompts)
+const departments = [];
+const employees = [];
+const managers = [];
+const roles = [];
+
 // Configure MySQL
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -21,115 +27,118 @@ connection.connect((err) => {
 });
 
 // Init
-updateEmployees();
 ask();
 
 // Primary logic routing function
 function ask() {
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'input',
-      choices: [
-        'View All Employees',
-        'Add New Employee',
-        'Update Employee Role',
-        'View All Roles',
-        'Add New Role',
-        'View All Departments',
-        'Add New Department',
-        'Exit',
-      ],
-      message: 'What would you like to do?',
-    },
-  ]).then((answer) => {
-    const { input } = answer;
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'input',
+        choices: [
+          'View All Employees',
+          'Add New Employee',
+          'Update Employee Role',
+          'View All Roles',
+          'Add New Role',
+          'View All Departments',
+          'Add New Department',
+          'Exit',
+        ],
+        message: 'What would you like to do?',
+      },
+    ])
+    .then((answer) => {
+      const { input } = answer;
 
-    switch (input) {
-      case 'View All Employees':
-        viewData(query.viewEmployees);
-        break;
-      case 'Add New Employee':
-        addEmployee();
-        break;
-      case 'Update Employee Role':
-        break;
-      case 'View All Roles':
-        viewData(query.viewRoles);
-        break;
-      case 'Add New Role':
-        addRole();
-        break;
-      case 'View All Departments':
-        viewData(query.viewDepts);
-        break;
-      case 'Add New Department':
-        addDepartment();
-        break;
-      case 'Exit':
-        exit();
-        break;
-      default:
-        console.log('Something went wrong. Please ask again.');
-        ask();
-    }
-  });
+      switch (input) {
+        case 'View All Employees':
+          viewData(query.viewEmployees);
+          break;
+        case 'Add New Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          break;
+        case 'View All Roles':
+          viewData(query.viewRoles);
+          break;
+        case 'Add New Role':
+          addRole();
+          break;
+        case 'View All Departments':
+          viewData(query.viewDepts);
+          break;
+        case 'Add New Department':
+          addDepartment();
+          break;
+        case 'Exit':
+          exit();
+          break;
+        default:
+          console.log('Something went wrong. Please ask again.');
+          ask();
+      }
+    });
 }
 
 // Add employee to database
 function addEmployee() {
 
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: "Employee's First Name:",
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: "Employee's Last Name:",
-    },
-    {
-      type: 'list',
-      name: 'role_id',
-      choices: roles,
-      message: "Employee's Role:",
-    },
-    {
-      type: 'list',
-      name: 'manager_id',
-      choices: managers,
-      message: "Employee's Manager:",
-    },
-  ]).then((answers) => {
+  // Update local roles and managers array used in the prompts below
+  updateRoleData(query.updateRoleData);
+  updateManagerData(query.updateManagerData);
 
+  // Collect new employee information from user
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: "Employee's First Name:",
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: "Employee's Last Name:",
+      },
+      {
+        type: 'list',
+        name: 'role_id',
+        choices: roles,
+        message: "Employee's Role:",
+      },
+      {
+        type: 'list',
+        name: 'manager_id',
+        choices: managers,
+        message: "Employee's Manager:",
+      },
+    ])
+    .then((answers) => {
 
-  });
+      console.log(answers);
+
+    });
 }
 
-
-
-
-// Query function: View data
-function viewData(queryString) {
+// Query function: View data from database
+async function viewData(queryString) {
   console.log('Requesting data from database... \n');
 
-  const results = connection.query(queryString, (err, res) => {
-    if (err) throw err;
+  const results = await getData(queryString);
+  console.log('Data retrieved. View Below: \n');
+  console.table(results);
 
-    console.log('Data downloaded. View below: \n');
-    console.table(res);
-
-    ask();
-  });
+  ask();
 }
 
 // Update local roles array used as choices array in inquirer prompts
 async function updateRoleData(queryString) {
-  const results = await updateAppData(queryString);
-  
-  results.forEach(row => {
+  const results = await getData(queryString);
+
+  results.forEach((row) => {
     const { id, title } = row;
     roles.push({ value: id, name: title });
   });
@@ -137,11 +146,22 @@ async function updateRoleData(queryString) {
 
 // Update local managers array used as choices array in inquirer prompts
 async function updateManagerData(queryString) {
-  const results = await updateAppData(queryString);
+  const results = await getData(queryString);
 
-  results.forEach(row => {
+  results.forEach((row) => {
     const { id, first_name, last_name } = row;
-    managers.push({ value: id, name: `${first_name} ${last_name}`});
+    managers.push({ value: id, name: `${first_name} ${last_name}` });
+  });
+}
+
+// Helper: Promisify the retrieval of data from the database
+function getData(queryString) {
+  return new Promise((resolve, reject) => {
+    connection.query(queryString, 
+    (err, res) => {
+      if (err) reject(err);
+      resolve(res);
+    });
   });
 }
 
